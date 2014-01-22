714238a797662b9916fb19b31742cd2d5dccb235
byakk70 <gs_li_samet@hotmail.com>
2014-01-22 22:24:43 +0100

    jenkins

---
+++ src/main/java/eu/sgok/PackageName/App.java
@@ -0,0 +1,13 @@
+package eu.sgok.PackageName;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hierfffff heb ik Hello World veranderd" );
+    }
+}
